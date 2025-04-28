f7acda2562b75834f3ccbdad5013022a1b540790
Rafael COPPE <rafael.coppe.jr@gmail.com>
2025-04-28 17:25:20 +0200

    Added bootstrap links to Pagefunction

--- pkg/page/page.go
+++ pkg/page/page.go
@@ -7,6 +7,8 @@ import (
 
 // Page génère une page HTML complète avec un titre et un corps de page.
 func Page(title string, bodyElements []string) string {
+	bootstrap := `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">`
 	body := fmt.Sprintf("<body>%s</body>", strings.Join(bodyElements, "\n"))
-	return fmt.Sprintf("<!DOCTYPE html><html><head><title>%s</title></head>%s</html>", title, body)
+	return fmt.Sprintf("<!DOCTYPE html><html><head>%s<title>%s</title></head>%s</html>", bootstrap, title, body)
 }
