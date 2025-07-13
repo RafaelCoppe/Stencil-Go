ee54e8a5479051e47d0ab9a8c81bc761713d4ba1
Rafael COPPE <rafael.coppe.jr@gmail.com>
2025-07-13 11:54:14 +0200

    Small update on Bouton function

--- pkg/interactions/bouton.go
+++ pkg/interactions/bouton.go
@@ -2,14 +2,14 @@ package StencilInteractions
 
 import (
 	"fmt"
-	"github.com/RafaelCoppe/Stencil-Go/pkg/utils"
+	"strings"
 )
 
 // Button génère un bouton HTML.
-func Bouton(label string, event string, classes []string) string {
+func Bouton(text string, event string, classes ...string) string {
 	return fmt.Sprintf(`<button data-onclick="%s" class="%s">%s</button>`,
 		event,
-		StencilUtils.Join(classes...),
-		label,
+		strings.Join(classes, " "),
+		text,
 	)
 }
